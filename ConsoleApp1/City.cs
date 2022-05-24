a8f4b5e98cd61d0e15751ad25bdfce6641ea4a10
Mikhail <basargin123@gmail.com>
2022-05-24 20:31:12 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp1/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public partial class City
+    {
+        public City()
+        {
+            Clubs = new HashSet<Club>();
+        }
+
+        public long CityId { get; set; }
+        public string? Title { get; set; }
+        public string? State { get; set; }
+        public long? Population { get; set; }
+
+        public virtual ICollection<Club> Clubs { get; set; }
+    }
+}
